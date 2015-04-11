947b160a5cb4126257a50a31a6eb77b4fa901845
Aaron Bembenek <bembenek@g.harvard.edu>
2015-04-11 17:50:23 -0400

    updated fake harness with function pointers

--- fake_harness.cpp
+++ fake_harness.cpp
@@ -2,6 +2,11 @@
 #include <cstdio>
 #include <cassert>
 
+#define TRACE_DEPTH 2
+#define ORTHO_DEPTH 2
+
+typedef int (*int_func)();
+
 int old_x = 0, old_y = 0, old_z = 0;
 int new_x = 0, new_y = 0, new_z = 0;
 
@@ -33,34 +38,40 @@ int new_g() {
   return --new_x;
 }
 
-int main() {
-  int ns[5];
-  klee_make_symbolic(&ns, sizeof(ns), "ns");
+int_func old_fs[3] = { &old_f1, &old_f2, &old_f3 };
+int_func new_fs[3] = { &new_f1, &new_f2, &new_f3 };
 
-  int *p;
-  int d = 0;
-  for (p = ns; p < &ns[5]; ++p) {
-    d = d * 10;
-    int x = *p % 4;
-    if (x == 0) {
-      if (old_f1() != new_f1()) goto FAILURE;
-      d += 1;
-    } else if (x == 1) {
-      if (old_f2() != new_f2()) goto FAILURE;
-      d += 2 ;
-    } else if (x == 2){
-      if (old_f3() != new_f3()) goto FAILURE;
-      d += 3;
-    } else {
+int main() {
+  unsigned int fs[TRACE_DEPTH];
+  klee_make_symbolic(&fs, sizeof(fs), "fs");
+
+  unsigned int *p;
+  unsigned int o = 0;  // stupid hack for recording interleavings
+  for (p = fs; p < &fs[TRACE_DEPTH]; ++p) {
+    o *= 10;
+
+    unsigned char gs;
+    klee_make_symbolic(&gs, sizeof(gs), "gs");
+    klee_assume(gs <= ORTHO_DEPTH);
+    int i;
+    for (i = 0; i < gs; ++i) {
       new_g();
-      d += 4;
-    }      
+      o += 4;
+      o *= 10;
+    }
+
+    unsigned int x = *p % 2;
+    o += x + 1;
+    int old_r = old_fs[x]();
+    int new_r = new_fs[x]();
+    // uncomment to short-circuit
+    // if (old_r != new_r) goto FAILURE;
   }
 
-  printf("%d\n", d);
+  printf("%d\n", o);
   return 0;
 
   FAILURE:
-  printf("%d\n", d);
-  assert(0);
+  printf("%d\n", o);
+  klee_assert(0);
 }
