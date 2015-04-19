775016f08577e839cb75ff1f924d9b4ac50c73b4
Aaron Bembenek <bembenek@g.harvard.edu>
2015-04-19 18:15:21 -0400

    implemented BigIntBag

--- bag/BigIntBag.hpp
+++ bag/BigIntBag.hpp
@@ -2,19 +2,20 @@
 #define _xBIG_INT_BAG_HPP_
 
 #include "IntBag.hpp"
+#include "BinaryTree.hpp"
 
 class BigIntBag : public IntBag {
   public:
-    BigIntBag();
-    ~BigIntBag();
-    bool member(int);
-    void insert(int);
-    void remove(int);
-    unsigned get_size();
-    unsigned to_array(int*&);
+    BigIntBag() {}
+    ~BigIntBag() {}
+    bool member(int x) { return bt.search(x); }
+    void insert(int x) { bt.add(x); }
+    void remove(int x) { bt.drop(x); }
+    unsigned get_size() { return bt.get_size(); }
+    unsigned to_array(int*& dest) { dest = 0; return 0; }
 
   private:
-    // ...
+    BinaryTree bt; 
 };
 
 #endif
