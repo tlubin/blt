2371c836ffe316bee3aefeab12dde0f284330858
tslilyai <Lily Tsai>
2015-04-25 18:12:52 -0700

    rand arg generator working

--- arggen.cpp
+++ arggen.cpp
@@ -1,9 +1,8 @@
 #include <climits>
+#include <cfloat>
 #include <cstdio>
 #include <cstdlib>
-#include <random>
-
-srand(seed);
+#include <string>
 
 static const char alphanum[] =
 "0123456789"
@@ -11,70 +10,109 @@ static const char alphanum[] =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 "abcdefghijklmnopqrstuvwxyz";  
 
-char genRandom() { return alphanum[rand() % stringLength]; }
+char genRandom() { return alphanum[rand() % (sizeof(alphanum) - 1)]; }
 
-void* get_arg(char* arg_type) {
-  ints = malloc(sizeof(int)*5);
-  uints = malloc(sizeof(int)*3);
-  llongs = malloc(sizeof(int)*5);
-  chars = malloc(sizeof(int)*5);
-  strings = malloc(sizeof(char*)*3);
-  
-  ints[0] = rand() % INT_MAX;
-  ints[1] = INT_MIN + rand() % INT_MAX;
-  ints[2] = INT_MAX;
-  ints[3] = INT_MIN;
-  ints[4] = 0;
- 
-  uints[0] = rand() % UINT_MAX;
-  uints[1] = UINT_MAX;
-  uints[2] = 0;
- 
-  llongs[0] = rand() % LLONG_MAX;
-  llongs[1] = LLONG_MIN + rand() % LLONG_MAX;
-  llongs[2] = LLONG_MAX;
-  llongs[3] = LLONG_MIN;
-  llongs[4] = 0;
+enum string_code { eint, euint, ellong, efloat, echar, estring };
 
-  chars[0] = rand() % CHAR_MAX;
-  chars[1] = CHAR_MIN + rand() % CHAR_MAX;
-  chars[2] = ' ';
-  chars[3] = CHAR_MAX;
-  chars[4] = CHAR_MIN;
+string_code hashit (std::string const& inString) {
+    if (inString == "int" || inString == "long") return eint;
+    if (inString == "unsigned") return euint;
+    if (inString == "long long") return ellong;
+    if (inString == "float") return efloat;
+    if (inString == "char") return echar;
+    if (inString == "char*") return estring;
+}
 
-  strings[0] = NULL;
-  strings[1] = "";
-  r = rand() % INT_MAX;
-  strings[2] = malloc(sizeof(char) * r);
-  for(unsigned i = 0; i < r; ++i)
-      strings[2][i] = genRandom();
-  
+void* get_arg(std::string const& arg_type) {
   void* ret;
-  switch (arg_type) {
-    case "int":
-    case "long":
-      ret = (void*)&ints[rand() % 5];
-      break;
-    case "uint":
-      ret = (void*)&uints[rand() % 3];
-      break;
-    case "llong":
-      ret = (void*)&llongs[rand() % 5];
-      break;
-    case "char":
-      ret = (void*)&chars[rand() % 5];
-      break;
-    case "char*":
-      ret = (void*)&strings[rand() % 3];
-      break;
+  switch (hashit(arg_type)) {
+    case eint: {
+      int ints[4];
+        ints[0] = INT_MIN + rand() /(RAND_MAX/(INT_MAX-INT_MIN));
+        ints[1] = INT_MAX;
+        ints[2] = INT_MIN;
+        ints[3] = 0;
+      int* ret = new int[1];
+      ret[0] = ints[rand() / (RAND_MAX/4)];
+      return (void*)ret;
+    }
+    case euint: {
+      unsigned uints[3];
+        uints[0] = rand() / (RAND_MAX/UINT_MAX + 1);
+        uints[1] = UINT_MAX;
+        uints[2] = 0;
+      unsigned* ret = new unsigned[1];
+      ret[0] = uints[rand() / (RAND_MAX/3)];
+      return (void*) ret;
+    }
+    case ellong: {
+      long long llongs[4];
+        llongs[0] = LLONG_MIN + rand() /(RAND_MAX/(LLONG_MAX-LLONG_MIN));
+        llongs[1] = LLONG_MAX;
+        llongs[2] = LLONG_MIN;
+        llongs[3] = 0;
+      long long* ret = new long long[1];
+      ret[0] = llongs[rand() / (RAND_MAX/4)];
+      return (void*) ret;
+    }
+    case efloat: {
+      float floats[3];
+        floats[0] = FLT_MIN + rand() /(RAND_MAX/(FLT_MAX-FLT_MIN));
+        floats[1] = FLT_MAX;
+        floats[2] = FLT_MIN; floats[3] = 0;
+      float* ret = new float[1];
+      ret[0] = floats[rand() / (RAND_MAX/4)];
+      return (void*) ret;
+    }
+    case echar: {
+      char chars[4];
+        chars[0] = CHAR_MIN + rand() /(RAND_MAX/(CHAR_MAX-CHAR_MIN));
+        chars[1] = ' ';
+        chars[2] = CHAR_MAX;
+        chars[3] = CHAR_MIN;
+      char* ret = new char[1];
+      ret[0] = chars[rand() / (RAND_MAX/4)];
+      return (void*) ret; 
+    }
+    case estring: {
+      char* strings[3];
+        strings[0] = NULL;
+        strings[1] = (char*)"";
+        unsigned r = rand() % CHAR_MAX;
+        strings[2] = (char*) malloc(sizeof(char) * r);
+        for(unsigned i = 0; i < r; ++i)
+            strings[2][i] = genRandom();
+        strings[2][r] = 0;
+      char** ret = new char*[1];
+      ret[0] = strings[rand() / (RAND_MAX/3)];
+      return (void*) ret; 
+    }
     default:
       break;
   }
-  free(ints);
-  free(uints);
-  free(llongs);
-  free(chars);
-  free(strings[2]);
-  free(strings);
-  return ret;
 }
+
+/*
+int main() {
+  int seed = 383;
+  srand(seed);
+  printf("int: %d\n", *((int*)get_arg("int")));
+  printf("int: %d\n", *((int*)get_arg("int")));
+  printf("int: %d\n", *((int*)get_arg("int")));
+  printf("uint: %u\n", *((unsigned*)get_arg("unsigned")));
+  printf("uint: %u\n", *((unsigned*)get_arg("unsigned")));
+  printf("uint: %u\n", *((unsigned*)get_arg("unsigned")));
+  printf("llong: %lld\n", *(long long*)get_arg("long long"));
+  printf("llong: %lld\n", *(long long*)get_arg("long long"));
+  printf("llong: %lld\n", *(long long*)get_arg("long long"));
+  printf("float: %f\n", *(float*)get_arg("float"));
+  printf("float: %f\n", *(float*)get_arg("float"));
+  printf("float: %f\n", *(float*)get_arg("float"));
+  printf("char: %c\n", *(char*)get_arg("char"));
+  printf("char: %c\n", *(char*)get_arg("char"));
+  printf("char: %c\n", *(char*)get_arg("char"));
+  printf("string: %s\n", *(char**)get_arg("char*"));
+  printf("string: %s\n", *(char**)get_arg("char*"));
+  printf("string: %s\n", *(char**)get_arg("char*"));
+}
+*/
